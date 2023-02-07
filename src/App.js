b76120810242cd8bd2f1f99b6bd50f59a07f028a
D.N.Singh <76054685+DNSingh-15@users.noreply.github.com>
2023-02-07 17:10:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Calculator from './Claculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Calculator/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
